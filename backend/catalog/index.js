9bc16c552e507e411ce9a1bf36a27829da9f835b
gsjurseth <geir.sjurseth@gmail.com>
2018-02-13 14:21:13 +0100

    committing changes that make this work with oauth .. config.json not checked in on purpose

--- backend/catalog/index.js
+++ backend/catalog/index.js
@@ -1,33 +1,75 @@
 const Promise = require('bluebird'),
   express     = require('express'),
-  fetch       = require('node-fetch');
+  fetch       = require('node-fetch'),
+  cache       = require('node-cache'),
+  config      = require('./config.json');
 
+/*
+ * config.json isn't checked into the repo. It's a simple json
+ * {
+ *   "client_id" : "daClientIDString",
+ *   "secret": "daClientSecret"
+ * }
+ */
 
 fetch.Promise = Promise;
 
 /* some express and runtime constants */
 const app = new express();
+const myCache = new cache( { stdTTL: 100, checkperiod: 120 } );
 const searchSvc   = 'http://gsjurseth-eval-test.apigee.net/inventory/v1/trashcans/';
 const pricingSvc  = 'http://gsjurseth-eval-test.apigee.net/shopping/v1/pricing/trashcans';
 
 
-app.get('/catalog', (req,res) => {
-  fetch(pricingSvc)
-    .then( d => d.json() )
-    .map( d => {
-      return fetch( `${searchSvc}${d.name}` )
-        .then( r => r.json()  )
-        .then( r => {
-          console.log('here I am with the r: %j', r);
-          return Object.assign({}, r, {price: d.price});
+function daLoginStuff() {
+  return new Promise(  (res,rej) => {
+    let OAuthSet = myCache.get( "oauth" );
+    if ( OAuthSet && (OAuthSet.expires_in > 10) ) {
+      console.log("Returning cached oauth set");
+      res(OAuthSet);
+    }
+    else {
+      fetch( config.tokenUrl, { 
+        method: 'POST', 
+        headers: { "Authorization": Buffer.from( config.client_id + ":" + config.secret, "utf8").toString("base64") }
+      })
+        .then( d => d.json() )
+        .then( d => {
+          console.log("Returning fresh oauth set");
+          myCache.set( "oauth", d, (d.expires_in - 10) )
+          res(d);
         });
-    })
+    }
+  });
+}
+
+app.get('/login', (req,res) => {
+  daLoginStuff()
     .then( d => {
-      return res.json( d );
-    })
-    .catch( e => {
-      console.error('We failed: %s', e.stack);
-      res.status(500).json({msg: "failed running set()", stack: e.stack})
+      res.json(d);
+    });
+});
+
+app.get('/catalog', (req,res) => {
+  daLoginStuff()
+    .then( auth => {
+      return fetch(pricingSvc, { headers: { "Authorization": `Bearer ${auth.access_token}` } })
+        .then( d => d.json() )
+        .map( d => {
+          return fetch( `${searchSvc}${d.name}`, { headers: { "Authorization": `Bearer ${auth.access_token}` } } )
+            .then( r => r.json()  )
+            .then( r => {
+              console.log('here I am with the r: %j', r);
+              return Object.assign({}, r, {price: d.price});
+            });
+        })
+        .then( d => {
+          return res.json( d );
+        })
+        .catch( e => {
+          console.error('We failed: %s', e.stack);
+          res.status(500).json({msg: "failed running set()", stack: e.stack})
+        });
     });
 });
 
