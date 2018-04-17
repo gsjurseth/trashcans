5bb671c5078af6b87ffd8b5631f1b1851f7da294
gsjurseth <geir.sjurseth@gmail.com>
2018-04-17 10:40:56 +0200

    added foo

---
+++ frontend/bm/src/components/Foo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Header, Image } from 'semantic-ui-react'
+import Trashcans from "./Trashcans";
+import Navbar from "./Navbar";
+
+
+const App = () => (
+
+  <div>
+      <Navbar />
+      <i class="reddit icon" />
+      I am foo. Hear me roar!
+      <i class="snapchat icon" />
+  </div>
+
+);
+
+export default App;
